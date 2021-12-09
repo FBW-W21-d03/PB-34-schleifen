3abe4d2ab5175c6617ee3983bf5d98f54ad11d25
slavablack <veaceslav.ciornii@magenta.de>
2021-12-09 16:18:34 +0100

    Aufgabe mit schleifen

---
+++ index.js
@@ -0,0 +1,26 @@
+ //1
+ let i=0;
+
+ while (i<=15) {
+  
+    console.log("i=" ,i);  
+     i++;//i=i+1
+ }
+ //2 
+ let num = 1;
+ let summe=0;
+ while(num<=20) {
+  
+     
+     summe=summe+num;
+    num=num +1;
+ }
+ console.log(summe)
+ 
+//3
+let ii=0;
+do { 
+
+    console.log("Die zahl ist:", ii)
+    ii=ii+1;
+} while (ii<20);
