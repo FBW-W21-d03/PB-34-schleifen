7f1aa944631392df7012fec026173cfbb2dc686c
kawanaza <kawanbabani@gmail.com>
2021-12-09 14:39:56 +0100

    Done

---
+++ main.js
@@ -0,0 +1,30 @@
+let i = 0;
+
+while (i < 15) {
+  i = i + 1;
+  console.log("", i);
+}
+
+const meinZhal = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+let summary = 0;
+for (let index = 0; index < meinZhal.length; index++) {
+  summary = summary + meinZhal[index];
+}
+console.log("meinZhal ist : ", summary);
+
+const zhalen = i;
+
+while (zhalen <= 20) {
+  zhalen = zhalen + 1;
+  console.log(zhalen);
+}
+
+i = 0;
+
+do {
+  console.log("MeinZhal Ist :  " + i);
+  i++;
+} while (i <= 20);
+
