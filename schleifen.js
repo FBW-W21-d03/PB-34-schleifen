13df64459ff76ba40d1d73832b9c7017a42c23bc
Salimov333 <salimov333@gmail.com>
2021-12-09 15:41:35 +0100

    Salem lösung

--- schleifen.js
+++ schleifen.js
@@ -2,26 +2,23 @@ let i = 0;
 while (i < 15) {
     console.log("i = ", i);
     i++;
-    
 }
 
 const sum1to20 = () => {
     let sum = 0;
-    let counter = 0;
-    while (counter <20) {
+    let counter = 1;
+    while (counter <=20) {
         sum = sum + counter;
         counter++;
     }
-    
     return sum;
 }
 
-console.log(sum1to20()); //190
+console.log(sum1to20()); // 210
 
 
-let a = 0;
 do {
-    console.log("a = ", + a);
-    a++;
+    console.log("Die zahl ist = ", + i);
+    i++;
 } 
-while (a < 20);
+while (i < 20);
