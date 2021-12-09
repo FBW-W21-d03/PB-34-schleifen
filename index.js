197141e8f0bee61a77e9728e02f102d69dfd6bde
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-09 15:06:38 +0100

    js

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Teil 1 )
+let i = 0;
+
+while ( i < 15) {
+  
+  i = i + 1;
+  console.log(i);
+}
+
+
+
+
+
+// Teil 2 )
+let sum = 0; 
+i = 1; 
+while (i <= 20) {
+  sum = sum + i;
+  i = i + 1;
+}
+console.log(sum);
+
+console.log(i);
+
+// Teil 3 )
+i = 0;
+do {
+  console.log("Die Zahl ist", i);
+  i = i + 1;
+} while (i < 20);
