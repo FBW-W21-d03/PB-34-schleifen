7733ac81c116d81ab5de3f6beaacaa47e6074896
ShiinyFlakes <mago_elm@hotmail.com>
2021-12-09 14:10:45 +0100

    Lösung

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let wert = 0;
+while (wert < 15){
+    wert++; 
+    console.log(wert);
+}
+
+const zahlen = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+let summe = 0;
+for(let index = 0; index < zahlen.length; index++) {
+    summe = summe + zahlen[index];
+  }
+  console.log(summe);
+
+ let i = 0;
+
+do {
+    i++;
+    console.log("Die zahl ist", + i);
+} 
+while (i < 19)
