4378826afce9ab4fab547a30b33de901bb61ebbe
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-12-09 16:37:37 +0100

    Orhan's lösung

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Aufgabe 1: ");
+
+let i = 0;
+
+while (i < 15) {
+    console.log(i++);
+}
+
+console.log("Aufgabe 2: ");
+
+let sum = 0;
+let start = 1;
+
+while (start <= 20) {
+    sum = sum + start;
+    start++;
+}
+
+console.log(sum);
+
+
+console.log("Aufgabe 3: ");
+
+let j = 0;
+
+do {
+    console.log("Zahl ist:", j);
+    j++;
+} while (j < 20);
