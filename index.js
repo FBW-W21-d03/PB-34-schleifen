b17ce18001859558d9cf80fa5ff5b3b90db45188
sprotte <metatron1972@live.de>
2021-12-10 08:45:59 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,26 @@
+// Zu 1
+let i = 0;
+while (i < 15) {
+  console.log(i);
+  i++;
+}
+
+console.log("**********");
+
+// Zu 2
+let sum = 0;
+i = 1; 
+while (i <= 20) {
+  sum = sum + i;
+  i++;
+}
+console.log(sum);
+
+console.log("**********");
+
+// Zu 3
+i = 0;
+do {
+  console.log("Zahl ist", i);
+  i++;
+} while (i < 20);
